5bcc111a9cc9e7f9c706ce6b1933670d47703475
Jcarri14 <j.carrion@students.salle.url.edu>
2021-07-15 12:49:35 +0200

    feat: add new pages refs

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import { Switch, Route } from "react-router-dom";
 import "./styles/App.css";
 
 import Home from "./pages/Home";
+import Register from "./pages/Register";
 import Login from "./pages/Login";
+import ResetPassword from "./pages/ResetPassword";
 
 import AuthContext from "./context/AuthContext";
 import { auth } from "./services/auth";
@@ -39,6 +41,16 @@ function App() {
             path="/login"
             exact
             render={(routeProps) => <Login {...routeProps} />}
+          />
+          <Route
+            path="/sign-up"
+            exact
+            render={(routeProps) => <Register {...routeProps} />}
+          />
+          <Route
+            path="/reset-password"
+            exact
+            render={(routeProps) => <ResetPassword {...routeProps} />}
           />
           <Route path="/" render={(routeProps) => <Home {...routeProps} />} />
         </Switch>
